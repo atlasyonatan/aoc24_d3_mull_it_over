91133fad47a89249d8660b58361a1588a1d9573a
Yonatan Atlas <atlasyonatan@gmail.com>
2024-12-20 20:50:53 +0200

    working p2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use regex::Regex;
 use std::io::{self, Read};
 
@@ -5,18 +6,69 @@ fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
 
-    let reg = Regex::new(r"mul\((\d{1,3})\,(\d{1,3})\)").unwrap();
-    let muls = reg.captures_iter(&input).map(|captures| {
-        let mut iter = captures
-            .iter()
-            .skip(1)
-            .map(Option::unwrap)
-            .map(|s| s.as_str().parse::<u16>().unwrap());
+    let reg = Regex::new(r"(?<mul>mul\((\d{1,3})\,(\d{1,3})\))|(?<do>do\(\))|(?<dont>don\'t\(\))")
+        .unwrap();
+    let names: Vec<_> = reg.capture_names().skip(1).filter_map(|c| c).collect();
+    let commands: Vec<_> = reg
+        .captures_iter(&input)
+        .map(|captures| {
+            let name = names
+                .iter()
+                .find(|name| captures.name(name).is_some())
+                .unwrap();
 
-        (iter.next().unwrap(), iter.next().unwrap())
-    });
+            return match *name {
+                "mul" => {
+                    let (a, b) = captures
+                        .iter()
+                        .skip(2) //skip full group and named group
+                        .map(Option::unwrap)
+                        .map(|s| s.as_str())
+                        .map(|s| s.parse().unwrap())
+                        .next_tuple()
+                        .unwrap();
 
-    let result: u32 = muls.map(|(n1, n2)| n1 as u32 * n2 as u32).sum();
+                    Command::Mul(a, b)
+                }
+                "do" => Command::Do,
+                "dont" => Command::Dont,
+                _ => unreachable!(),
+            };
+        })
+        .collect();
+
+    let result: u32 = commands
+        .iter()
+        .filter_map(|command| match command {
+            Command::Mul(a, b) => Some((*a as u32) * (*b as u32)),
+            _ => None,
+        })
+        .sum();
 
     println!("Part 1: {}", result);
+
+    let result: u32 = commands
+        .into_iter()
+        .fold((true, 0), |(mut enabled, mut sum), command| {
+            match command {
+                Command::Mul(a, b) if enabled => {
+                    sum += (a as u32) * (b as u32);
+                }
+                Command::Do => enabled = true,
+                Command::Dont => enabled = false,
+                _ => {}
+            }
+
+            (enabled, sum)
+        })
+        .1;
+
+    println!("Part 2: {}", result);
+}
+
+#[derive(Debug)]
+enum Command {
+    Mul(u16, u16),
+    Do,
+    Dont,
 }
